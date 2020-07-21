c49950fb3f3d8ece5431f43c41e7c30757ad72d6
sixteen <yinyazuolun@gmail.com>
2020-07-21 23:25:05 +0800

    接通数据库

--- src/main/java/com/sixteen/school/model/Teacher.java
+++ src/main/java/com/sixteen/school/model/Teacher.java
@@ -1,8 +1,14 @@
 package com.sixteen.school.model;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.Data;
 
+
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Data
 @Table(name = "teacher")
@@ -10,6 +16,21 @@ public class Teacher {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    @Column(name="`name`")
-    private String name;
+    @Column(name="TeacherName")
+    private String teacherName;   /**
+     * 创建时间
+     */
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @Column(name = "CreationTime", updatable = false)
+    private LocalDateTime creationTime = LocalDateTime.now();
+
+    /**
+     * 变更时间
+     */
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @Column(name = "LastModificationTime")
+    private LocalDateTime lastModificationTime = LocalDateTime.now();
+
 }
